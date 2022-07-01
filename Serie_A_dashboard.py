7968fb10d4528858c82baf71345a4e9179d5460f
Ed Morris <102766519+datawithed@users.noreply.github.com>
2022-07-01 15:07:31 +0100

    Update Serie_A_dashboard.py

--- Serie_A_dashboard.py
+++ Serie_A_dashboard.py
@@ -350,7 +350,7 @@ if add_sidebar == 'Intro':
                 \nAims of this dashboard:
                 \n- This is a brief dashboard demonstrating Wyscout event data manipulation and corresponding data visualisations using the free Wyscout event data for the Serie A match Juventus - Sampdoria on 10th September 2020.
                 \n- Here, I hope to demonstrate my ability to use this event data to create visually appealing graphics that can be used to analyse individual player performance, team performance and provide data-driven metrics where appropriate.
-                \n- The data used for this can be found here: https://footballdata.wyscout.com/download-samples/
+                - The data used for this can be found here: https://footballdata.wyscout.com/download-samples/
                 """)
 
 
